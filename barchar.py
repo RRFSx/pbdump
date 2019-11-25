28df57e679cb8d1dcc9c20af15c7d2fbd163b6f0
guoqing.ge <Guoqing.Ge@noaa.gov>
2019-11-25 12:17:24 -0700

    changes on barchar.py

--- barchar.py
+++ barchar.py
@@ -23,21 +23,6 @@ ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.legend()
 
-
-def autolabel(rects):
-    """Attach a text label above each bar in *rects*, displaying its height."""
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-
-
-autolabel(rects1)
-autolabel(rects2)
-
 fig.tight_layout()
 
 plt.show()
